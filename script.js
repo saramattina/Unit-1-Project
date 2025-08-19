8a4d8fb90645a93cf564e95b8558d8a9cfd744b7
Sara Mattina <smattina@gmail.com>
2025-08-19 10:26:18 -0400

    placePiece function working

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/** remember to clean up any unused variables, functions, etc... **/
+
 // Variables
 
 let board = [
@@ -22,12 +24,14 @@ let board = [
   "",
   "",
 ];
+
 let winner = false;
 let currentPlayer = "🔴";
 let redScore = 0;
 let blueScore = 0;
 
-//constants
+let spacesToMove;
+
 const squareEls = document.querySelectorAll(".sqr");
 const turnMessageEl = document.querySelector(".turn-message");
 const rollMessageEl = document.querySelector(".roll-message");
@@ -47,47 +51,93 @@ const stickFour = document.querySelector("#stick-four");
 const playerRed = "🔴";
 const playerBlue = "🔵";
 
-const turnMessage = `It's ${currentPlayer}'s turn!`;
+const turnMessage = () => `It's ${currentPlayer}'s turn!`;
 const winMessage = `${currentPlayer} has won!`;
-
-const doMessage = "You rolled a do! Move forward 1 space!"; //<--- 3 round, 1 flat up
-const geMessage = "You rolled a ge! Move forward 2 spaces!"; //<--- 2 round, 2 flat up
-const geolMessage = "You rolled a geol! Move forward 3 spaces!"; //<--- 1 round, 3 flat up
-const yutMessage = "You rolled a yut! Move forward 4 space!s"; //<--- 4 flat up + go again
-const moMessage = "You rolled a mo! Move forward 5 space!"; //<--- 4 round up + go again
 
 //functions
 
+const removeFlatClass = (e) => {
+  e.classList.remove("flat");
+};
+
 const init = () => {
+  board = [
+    "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+  ];
+  squareEls.forEach((sqr) => (sqr.textContent = ""));
   turnMessageEl.textContent = "Let's play! 🔴 goes first!";
   winner = false;
   currentPlayer = "🔴";
   playerRedScore.textContent = `Red: ${redScore}`;
   playerBlueScore.textContent = `Blue: ${blueScore}`;
   // console.log(turnMessageEl)
-  //need to remove .flat from sticks
+  removeFlatClass(stickOne);
+  removeFlatClass(stickTwo);
+  removeFlatClass(stickThree);
+  removeFlatClass(stickFour);
 };
 
-// init();
+//placePiece
+//maybe stretch goal: first roll is to place piece on board[0]
+//target current piece place with board[i]
+
+//add spaces to move to current board[i]
+//place piece in new board[i]
+//remove piece from previous board[i] to prevent having multiple pieces on board
+
+const findPiece = (squareEls) => {
+   // i is never passed in here
+  return squareEls.id;
+};
+
+let pieceIdx;
+let currentPieceSqr;
+
+const placePiece = () => {
+   // currentPieceSqr is always -1 because we're not passing in argument to findPiece
+  currentPieceSqr = board.findIndex(findPiece);
+  pieceIdx = (currentPieceSqr + spacesToMove);
+
+  board[pieceIdx] = currentPlayer;
+
+
+  console.log("current piece sqr" + currentPieceSqr)
+  console.log("new piece idx:" + pieceIdx);
+
+  //   console.log(board.findIndex(findPiece))
+  updateBoard();
+};
 
 const updateBoard = () => {
+//    console.log(board.findIndex(placePiece));
   board.forEach((sqr, idx) => {
     squareEls[idx].textContent = sqr;
   });
 };
+
+// console.log(updateBoard)
 //^taken from my tic tac toe lab
 
-const placePiece = (i) => {
-  board[i] = currentPlayer;
-  updateBoard();
-};
-//^taken from my tic tac toe lab
+//is there a way to keep only the last red or blue in the array?
+//reverse array, filter or find for first value, reverse array again
+//or use indexOf in some way?
+//findLastIndex() ?
+
+// const reverseBoard = board.reverse();
+// reverseBoard.filter()
+// board = reverseBoard.reverse();
+// console.log(reverseBoard)
 
 const switchPlayerTurn = () => {
   currentPlayer = currentPlayer === "🔴" ? "🔵" : "🔴";
   if (!winner) {
     turnMessageEl.innerText = turnMessage();
   }
+
+  removeFlatClass(stickOne);
+  removeFlatClass(stickTwo);
+  removeFlatClass(stickThree);
+  removeFlatClass(stickFour);
   // stick.classList.remove("round", "flat");
   // need remove flat or round class from sticks
 };
@@ -106,9 +156,6 @@ const rollSticks = (e) => {
   }
 };
 
-//if randomNum < 0.5, add class "round" to stick or have this be default
-//if randomNum > 0.5, add class "flat" to stick
-
 //event listeners
 
 rollBtn.addEventListener("click", () => {
@@ -117,20 +164,47 @@ rollBtn.addEventListener("click", () => {
   rollSticks(stickThree);
   rollSticks(stickFour);
 
-   if (numFlatSticks.length = 0) {
-      turnMessageEl.textContent = moMessage;
-   } else if (numFlatSticks.length = 1) {
-       turnMessageEl.textContent = doMessage;
-   } else if (numFlatSticks.length = 2) {
-      turnMessageEl.textContent = geMessage
-   } else if (numFlatSticks.length = 3) {
-      turnMessageEl.textContent = geolMessage;
-   } else {
-      turnMessageEl.textContent = yutMessage;
-   }
+  if (numFlatSticks.length === 0) {
+    turnMessageEl.textContent =
+      "You rolled a yut! Put a piece on the board or move forward 4 spaces!";
+    spacesToMove = 4;
+  } else if (numFlatSticks.length === 1) {
+    turnMessageEl.textContent =
+      "You rolled a do! Put a piece on the board or move forward 1 space!";
+    spacesToMove = 1;
+  } else if (numFlatSticks.length === 2) {
+    turnMessageEl.textContent =
+      "You rolled a ge! Put a piece on the board or move forward 2 spaces!";
+    spacesToMove = 2;
+  } else if (numFlatSticks.length === 3) {
+    turnMessageEl.textContent =
+      "You rolled a geol! Put a piece on the board or move forward 3 spaces!";
+    spacesToMove = 3;
+  } else {
+    turnMessageEl.textContent =
+      "You rolled a mo! Put a piece on the board or move forward 5 spaces!";
+    spacesToMove = 5;
+  }
 
+  placePiece();
+   //  updateBoard();
 
-  console.log(numFlatSticks.length);
+//   if (!winner) {
+//      switchPlayerTurn()};
+
+  //   console.log(numFlatSticks.length);
 });
+//*** ^when I add the if (!winner) switch turns, the rollBtn stops working
 
 resetBtn.addEventListener("click", init);
+
+init();
+
+//<dialog> for instructions?
+
+//first move
+//if no pieces are on the board, piece automatically gets placed on first square
+
+// const firstPiece = () => {
+
+// }
